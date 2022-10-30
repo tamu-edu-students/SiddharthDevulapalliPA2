7d861842a42efc2e37b4219f3c04e78593cd3a74
Siddharth Devulapalli <siddharthd@Siddharths-MacBook-Pro.local>
2022-10-30 13:04:26 -0500

    committing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+# end
+
+Rails.application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
 end
